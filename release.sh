03c2bfa98538f3458558e3ef875b32ae609087f6
Saif Ahmed <sahmed@kinetica.com>
2019-02-13 14:18:02 -0500

    KML-373 KAAW REST API Doesnt Parse HTTPD External URL

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build -f Dockerfile -t kinetica/kinetica-blackbox-sdk:r7.0.0.1-alpha . 
-docker push kinetica/kinetica-blackbox-sdk:r7.0.0.1-alpha
+docker build -f Dockerfile -t kinetica/kinetica-blackbox-sdk:r7.0.0.1-alpha2 . 
+docker push kinetica/kinetica-blackbox-sdk:r7.0.0.1-alpha2
