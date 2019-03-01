80e4372dc4a640c1e093cc660cd99b68b4494e01
Saif Ahmed <sahmed@kinetica.com>
2019-03-01 00:10:48 -0500

    Re-tagged SDK for 7.0.1 release, including Batch mode deployments

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build -f Dockerfile -t kinetica/kinetica-blackbox-sdk:r7.0.0.1-alpha2 . 
-docker push kinetica/kinetica-blackbox-sdk:r7.0.0.1-alpha2
+docker build -f Dockerfile -t kinetica/kinetica-blackbox-sdk:r7.0.1 . 
+docker push kinetica/kinetica-blackbox-sdk:r7.0.1
