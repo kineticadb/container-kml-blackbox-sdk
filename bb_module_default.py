6c0fda9334df079b7d835a9957f3ecd454359b82
Saif Ahmed <sahmed@kinetica.com>
2019-03-04 15:15:32 -0500

    Includes purposefully-slow calculation function to simulate long-running functions and test cross-worker distribution

--- bb_module_default.py
+++ bb_module_default.py
@@ -1,3 +1,6 @@
+import time
+import random
+
 def blackbox_function_default(inMap):
     outMap = inMap
     return outMap
@@ -37,3 +40,31 @@ def blackbox_function_advanced_math(inMap):
         'productinwords': mylabel + " " + str(int(f1*f2))
         }
     return outMap        
+
+def blackbox_function_math_superslow(inMap):
+    f1=int(inMap['figure1'])
+    f2=int(inMap['figure2'])
+    mylabel=inMap['mylabel']
+
+    # artificial delay
+    on_time_calcs_ratio = 0.99
+    if 'on_time_calcs_ratio' in inMap:
+        on_time_calcs_ratio=int(inMap['on_time_calcs_ratio'])
+
+    if random.random() >= 0.9999: # EXTREME SLOWDOWN!
+        time.sleep(60)
+    elif random.random() >= 0.999: # The 9-9-9 Plan!
+        time.sleep(15)
+    elif random.random() >= on_time_calcs_ratio: # Normal slowdown!
+        time.sleep(1)
+
+    if 'please_crash_me' in mylabel:
+        raise Exception("User-initiated purposeful failure! Go Figure")
+
+    outMap = {
+        'sum':f1+f2, 
+        'product':f1*f2,
+        'max':max([f1,f2]),
+        'suminwords': mylabel + " " + str(int(f1+f2))
+        }
+    return outMap     
