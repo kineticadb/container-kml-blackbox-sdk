ab9e115b9240bcbb9cc8265a61e4cd1977a55a59
Saif Ahmed <sahmed@kinetica.com>
2019-06-03 12:19:37 -0400

    Multi-record output enablement

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build -f Dockerfile -t kinetica/kinetica-blackbox-sdk:r7.0.1 . 
-docker push kinetica/kinetica-blackbox-sdk:r7.0.1
+docker build -f Dockerfile -t kinetica/kinetica-blackbox-sdk:r7.0.5b . 
+docker push kinetica/kinetica-blackbox-sdk:r7.0.5b
