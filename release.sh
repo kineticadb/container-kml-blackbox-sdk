75e67f103aa1f3e72543778b5bc241815719bf5f
sahmedkinetica <sahmed@kinetica.com>
2019-06-17 19:31:11 -0400

    Promotes candidate to v7.0.5

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build -f Dockerfile -t kinetica/kinetica-blackbox-sdk:r7.0.5b . 
-docker push kinetica/kinetica-blackbox-sdk:r7.0.5b
+docker build -f Dockerfile -t kinetica/kinetica-blackbox-sdk:r7.0.5 . 
+docker push kinetica/kinetica-blackbox-sdk:r7.0.5
