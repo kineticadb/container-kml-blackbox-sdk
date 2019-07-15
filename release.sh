d900b33a18f707677c67a76a1922eed56923b38e
sahmedkinetica <sahmed@kinetica.com>
2019-07-15 10:37:39 -0400

    Now includes independent repo designation file to more easily allow internal repos

--- release.sh
+++ release.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-docker build -f Dockerfile -t kinetica/kinetica-blackbox-sdk:r7.0.5 . 
+repo_uri=$(cat repo_uri.info)
+
+docker build -f Dockerfile -t $repo_uri . 
 
 if [ "$?" -eq "0" ]
 then
 	echo "Docker Build Successful, Publishing container publicly"
-	docker push kinetica/kinetica-blackbox-sdk:r7.0.5
+	docker push $repo_uri
 else
  	echo "Docker Build Failed, no release executed"
 fi
