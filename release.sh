5e0999198cc4d03b1ff1c88986a1770182180c55
sahmedkinetica <sahmed@kinetica.com>
2019-08-15 17:51:34 -0400

    Updated versioning and tagging script; bumped SDK version

--- release.sh
+++ release.sh
@@ -2,12 +2,16 @@
 
 repo_uri=$(cat repo_uri.info)
 
+echo "Building and publishing to $repo_uri"
+
 docker build -f Dockerfile -t $repo_uri . 
 
 if [ "$?" -eq "0" ]
 then
 	echo "Docker Build Successful, Publishing container publicly"
 	docker push $repo_uri
+	echo "Completed building and publishing to $repo_uri"
 else
  	echo "Docker Build Failed, no release executed"
 fi
+
