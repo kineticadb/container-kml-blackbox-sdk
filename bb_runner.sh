5abd712366467587f0ff15d6d97d77ab24e66eb6
sahmedkinetica <sahmed@kinetica.com>
2020-04-07 10:44:43 -0400

    Removed tee'd logging

--- bb_runner.sh
+++ bb_runner.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Running the primary model runner and passing in all arguments
-python -m sdk.bb_runner 2>&1 | tee bbox.log
+python -m sdk.bb_runner 2>&1
 
 # We should never really get here
 sleep 1000
